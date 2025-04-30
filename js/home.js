e66b8a5fffa780633f28ae3b2caa8f3051daa9e4
febriyanaf13 <febriyan.af13@gmail.com>
2025-04-30 12:03:47 +0700

    update : perbaikan tampilan sesuai figma

--- js/home.js
+++ js/home.js
@@ -8,7 +8,7 @@ function switchTab(tabId) {
     document.querySelectorAll('.content-section').forEach(section => {
         section.classList.remove('active');
     });
-    
+
     // Remove active class from all tabs
     document.querySelectorAll('.tab').forEach(tab => {
         tab.classList.remove('active');
@@ -20,13 +20,14 @@ function switchTab(tabId) {
     // Add active class to clicked tab
     event.target.classList.add('active');
 }
+
 function subrs(tabId) {
-    
+
     // Hide all content sections
     document.querySelectorAll('.subs-content').forEach(section => {
         section.classList.remove('active');
     });
-    
+
     // Remove active class from all tabs
     document.querySelectorAll('.tab').forEach(tab => {
         tab.classList.remove('active');
@@ -48,58 +49,62 @@ function rates2(event, tabId) {
 
 
     if (from && to && dates && ship) {
-        
         document.getElementById('from').textContent = from;
         document.getElementById('dates').textContent = dates;
         document.getElementById('to').textContent = to;
         document.getElementById('ship').textContent = ship;
         document.querySelectorAll('.content-section').forEach(section => {
-        section.classList.remove('active');
-    });
+            section.classList.remove('active');
+        });
         document.getElementById(tabId).classList.add('active');
+        document.getElementById('rates2').classList.add('show');
+
         document.querySelectorAll('.hero').forEach(hero => {
             hero.classList.remove('hero4');
             hero.classList.remove('hero3');
-        hero.classList.add('hero2');
-    });
+            hero.classList.add('hero2');
+        });
     } else {
         document.querySelectorAll('.content-section').forEach(section => {
-        section.classList.remove('active');
-    });
+            section.classList.remove('active');
+        });
         document.getElementById(tabId).classList.add('active');
         document.querySelectorAll('.hero').forEach(hero => {
             hero.classList.remove('hero4');
             hero.classList.remove('hero3');
-        hero.classList.add('hero2');
-    });
+            hero.classList.add('hero2');
+        });
     }
 }
+
 function rates3(event, tabId) {
     event.preventDefault();  // Prevent form submission
-        document.querySelectorAll('.content-section').forEach(section => {
+    document.querySelectorAll('.content-section').forEach(section => {
         section.classList.remove('active');
     });
-        document.getElementById(tabId).classList.add('active');
-        document.querySelectorAll('.hero').forEach(hero => {
+    document.getElementById(tabId).classList.add('active');
+    document.querySelectorAll('.hero').forEach(hero => {
         hero.classList.remove('hero4');
         hero.classList.remove('hero2');
         hero.classList.add('hero3');
 
     });
 }
+
 function rates4(event, tabId) {
     event.preventDefault();  // Prevent form submission
-        document.querySelectorAll('.content-section').forEach(section => {
+    document.querySelectorAll('.content-section').forEach(section => {
         section.classList.remove('active');
     });
-        document.getElementById(tabId).classList.add('active');
-        document.querySelectorAll('.hero').forEach(hero => {
+    document.getElementById(tabId).classList.add('active');
+    document.querySelectorAll('.hero').forEach(hero => {
         hero.classList.remove('hero3');
         hero.classList.remove('hero2');
         hero.classList.add('hero4');
 
     });
 }
+
 function checkTrack() {
     const resi = document.getElementById('res').value;
     document.getElementById('resi').textContent = resi;
@@ -111,3 +116,4 @@ function checkTrack() {
 
     });
 }
+
